a2d4277ef80298613c0e77f2b2a6c3f5fd1e69b2
Aleksandr Lepeshkin <allexllep@gmail.com>
2023-04-29 23:29:48 +0200

    add table creating scripts in mscew.sql

--- mscew.sql
+++ mscew.sql
@@ -1,1 +1,34 @@
-CREATE DATABASE IF NOT EXISTS mscew_database
+CREATE DATABASE IF NOT EXISTS mscew_database;
+USE mscew_database;
+
+CREATE TABLE IF NOT EXISTS mscew_songs (
+	song_id 	INT UNSIGNED NOT NULL AUTO_INCREMENT,	
+    title 		VARCHAR(255) NOT NULL,			
+    artist 		INT,							# foreign key to artist table
+    album 		INT,							# foreign key to album table
+    length 		LONG,							# length of song in seconds
+    PRIMARY KEY (song_id)
+);    
+    
+CREATE TABLE IF NOT EXISTS mscew_albums (
+	album_id 	INT UNSIGNED NOT NULL AUTO_INCREMENT,	
+    title 		VARCHAR(255) NOT NULL,			
+    artist 		INT,							
+    image		BLOB,
+    PRIMARY KEY (album_id)
+);        
+
+CREATE TABLE IF NOT EXISTS mscew_artists (
+	artist_id 	INT UNSIGNED NOT NULL AUTO_INCREMENT,	
+    title 		VARCHAR(255) NOT NULL,										
+    image		BLOB,
+    PRIMARY KEY (artist_id)
+);
+
+CREATE TABLE IF NOT EXISTS mscew_users (
+	user_id 	INT UNSIGNED NOT NULL AUTO_INCREMENT,	
+    login		VARCHAR(255) NOT NULL,			
+    email 		VARCHAR(255) NOT NULL,						
+    image		BLOB,
+    PRIMARY KEY (user_id)
+);  
