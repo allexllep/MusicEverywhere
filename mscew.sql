924c2eda55ff6191698098c46f28d496575e4125
Aleksandr Lepeshkin <allexllep@gmail.com>
2023-04-30 23:25:03 +0200

    add foregn key clauses in mscew.sql

--- mscew.sql
+++ mscew.sql
@@ -1,34 +1,86 @@
-CREATE DATABASE IF NOT EXISTS mscew_database;
-USE mscew_database;
+CREATE DATABASE IF NOT EXISTS mscew_db;
+USE mscew_db;
 
-CREATE TABLE IF NOT EXISTS mscew_songs (
-	song_id 	INT UNSIGNED NOT NULL AUTO_INCREMENT,	
-    title 		VARCHAR(255) NOT NULL,			
-    artist 		INT,							# foreign key to artist table
-    album 		INT,							# foreign key to album table
-    length 		LONG,							# length of song in seconds
-    PRIMARY KEY (song_id)
+CREATE TABLE IF NOT EXISTS songs (
+	song_id 		INT UNSIGNED NOT NULL AUTO_INCREMENT,	
+    album_id 		INT,
+    artist_id		INT,
+    song_title 		VARCHAR(255) NOT NULL,
+    song_length 	LONG,					# length of the song in seconds
+    song_number		INT,					# number of the song in the album
+    
+    PRIMARY KEY 	(song_id),
+    INDEX			(album_id),
+    INDEX			(artist_id),
+    
+    FOREIGN KEY		(album_id)
+		REFERENCES albums(album_id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+	
+    FOREIGN KEY		(artist_id)
+		REFERENCES artists(artist_id)
+        ON UPDATE CASCADE ON DELETE CASCADE
 );    
     
-CREATE TABLE IF NOT EXISTS mscew_albums (
-	album_id 	INT UNSIGNED NOT NULL AUTO_INCREMENT,	
-    title 		VARCHAR(255) NOT NULL,			
-    artist 		INT,							
-    image		BLOB,
-    PRIMARY KEY (album_id)
+CREATE TABLE IF NOT EXISTS albums (
+	album_id 		INT UNSIGNED NOT NULL AUTO_INCREMENT,	
+    artist_id 		INT,
+    album_title 	VARCHAR(255) NOT NULL,
+    album_year		YEAR,
+    album_image		BLOB,
+    
+    PRIMARY KEY 	(album_id),
+    INDEX			(artist_id),
+    
+    FOREIGN KEY		(artist_id)
+		REFERENCES artists(artist_id)
+        ON UPDATE CASCADE ON DELETE CASCADE
 );        
 
-CREATE TABLE IF NOT EXISTS mscew_artists (
-	artist_id 	INT UNSIGNED NOT NULL AUTO_INCREMENT,	
-    title 		VARCHAR(255) NOT NULL,										
-    image		BLOB,
-    PRIMARY KEY (artist_id)
+CREATE TABLE IF NOT EXISTS artists (
+	artist_id 		INT UNSIGNED NOT NULL AUTO_INCREMENT,	
+    artist_title 	VARCHAR(255) NOT NULL,										
+    artist_image	BLOB,
+    PRIMARY KEY 	(artist_id)
 );
 
-CREATE TABLE IF NOT EXISTS mscew_users (
-	user_id 	INT UNSIGNED NOT NULL AUTO_INCREMENT,	
-    login		VARCHAR(255) NOT NULL,			
-    email 		VARCHAR(255) NOT NULL,						
-    image		BLOB,
-    PRIMARY KEY (user_id)
+CREATE TABLE IF NOT EXISTS users (
+	user_id 		INT UNSIGNED NOT NULL AUTO_INCREMENT,	
+    login			VARCHAR(255) NOT NULL,			
+    email 			VARCHAR(255) NOT NULL,
+    user_password	VARCHAR(255) NOT NULL,
+    user_image		BLOB,
+    PRIMARY KEY 	(user_id),
+	UNIQUE			(login),
+    UNIQUE			(email)
 );  
+
+CREATE TABLE IF NOT EXISTS collections (
+	collection_id 		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	user_id				INT,
+    collection_title 	VARCHAR(255) NOT NULL,										
+    collection_image	BLOB,
+    
+    PRIMARY KEY 		(collection_id),
+    INDEX				(user_id),
+    
+    FOREIGN KEY			(user_id)
+		REFERENCES users(user_id)
+        ON UPDATE CASCADE ON DELETE CASCADE
+); 
+
+CREATE TABLE IF NOT EXISTS album_collection(
+	album_id 		INT,
+    collection_id 	INT,
+    PRIMARY KEY 	(album_id, collection_id),
+    INDEX			(album_id),
+	INDEX			(collection_id),
+    
+    FOREIGN KEY		(album_id)
+		REFERENCES albums(album_id)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+        
+    FOREIGN KEY		(collection_id)
+		REFERENCES collections(collection_id)
+        ON UPDATE CASCADE ON DELETE CASCADE   
+);
